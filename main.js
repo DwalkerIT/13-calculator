613d367bf02d9428dc9b9c04c0a1f47490f6941f
DWalkerIT <DWalkerIT@gmail.com>
2021-02-24 14:21:45 -0500

    updatedcalculator

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-const inputOne = document.querySelector("num_one");
-const inputTwo = document.querySelector("num_two");
+//DOM Manipulation
+const inputOne = document.querySelector("#num_one");
+const inputTwo = document.querySelector("#num_two");
 const answer = document.querySelector("#answer");
+const test = document.querySelector(".test");
+console.log(test.textContent);
 
 const btnContainer = document.querySelector("#btnContainer");
 
 function calculate(numOne, numTwo, action) {
   if (action == "add") {
-    answer.value = numOne + NumTwo;
+    answer.value = numOne + numTwo;
   } else if (action == "subtract") {
-    answer.value = numOne - NumTwo;
+    answer.value = numOne - numTwo;
   } else if (action == "multiply") {
-    answer.value = numOne * NumTwo;
+    answer.value = numOne * numTwo;
   } else if (action == "divide") {
-    answer.value = numOne / NumTwo;
+    answer.value = numOne / numTwo;
   }
 }
 
-const calculator = document.querySelector("calculator");
-
-calculator.addEventListener("submit", function (e) {
+const calculator = document.querySelector("#calculator");
+// console.log(calculator);
+btnContainer.addEventListener("click", function (e) {
   e.preventDefault();
+  const operator = e.target.id;
+  console.log(operator);
   const numOne = Number(inputOne.value);
   const numTwo = Number(inputTwo.value);
-  calculate(numOne, numTwo, action);
+  answer = calculate(numOne, numTwo, operator);
 });
+
+// btnContainer.addEventListener("click", function (e) {
+//   e.preventDefault();
+// });
 
 // btnContainer.addEventListener("click", function (e) {
 //   const currBtn = e.target;
@@ -66,31 +75,31 @@ calculator.addEventListener("submit", function (e) {
 // console.log(inputOne)
 // console.log(inputTwo)
 
-const btns = document.querySelectorAll("button");
+// const btns = document.querySelectorAll("button");
 
 // btns.addEventListener("click", function (e) {
 //   console.log(e.target);
 // });
 
 // const btnContainer = document.querySelector("#btn");
-document.querySelector("#calc").addEventListener("submit", function (e) {
-  e.preventDefault();
+// document.querySelector("#calc").addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-  let result = 0;
-  let value1 = document.querySelector("#userInput1").value;
-  let value2 = document.querySelector("#userInput2").value;
-  let operator = e.submitter.id;
-  value1 = Number(value1);
-  value2 = Number(value2);
-  if (operator == "plus") {
-    result = value1 + value2;
-  }
-  console.log(result);
-  document.querySelector("#result").value = result;
-  return;
-  // console.log(document.querySelector("input1").value);
-  // console.loga(e.submitter.id)
-});
+//   let result = 0;
+//   let value1 = document.querySelector("#userInput1").value;
+//   let value2 = document.querySelector("#userInput2").value;
+//   let operator = e.submitter.id;
+//   value1 = Number(value1);
+//   value2 = Number(value2);
+//   if (operator == "plus") {
+//     result = value1 + value2;
+//   }
+//   console.log(result);
+//   document.querySelector("#result").value = result;
+//   return;
+// console.log(document.querySelector("input1").value);
+// console.loga(e.submitter.id)
+// });
 
 // btnContainer.addEventListener("click", function (e) {
 //   const currEl = e.target;
